7c6d618841b3d98dbbcac0656e236ea158d05e57
Claudio Moreno Rosales <u20191e800@upc.edu.pe>
2024-11-01 03:41:49 -0500

    feat(main): updates

---
+++ src/main/java/com/edgeapi/service/fastporteiot/application/external/rest/SensorDataService.java
@@ -0,0 +1,15 @@
+package com.edgeapi.service.fastporteiot.application.external.rest;
+
+import com.edgeapi.service.fastporteiot.domain.model.valueobjects.SensorReading;
+import lombok.Value;
+import org.springframework.beans.factory.annotation.Autowired;
+import org.springframework.http.ResponseEntity;
+import org.springframework.stereotype.Service;
+import org.springframework.web.client.RestTemplate;
+
+import java.time.ZoneId;
+
+@Service
+public class SensorDataService {
+
+}
